bdcb772c7563d26ca40aa8006ad457c662424475
Jerald Austero <jerald.austero@tawk.to>
2021-12-02 23:55:46 +0800

    Add initial files and initial plugin library scripts

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# tawk-messenger-react
-React plugin for Tawk messenger
+<p align="center">
+    <a href="https://www.tawk.to/"
+    target="_blank"
+    rel="noopener noreferrer">
+        <img width="120"
+            src="https://www.tawk.to/wp-content/uploads/2020/04/tawk-sitelogo.png"
+            alt="Tawk logo">
+    </a>
+</p>
+
+<h1 align="center">
+    Tawk messenger for React
+</h1>
+
+> A plugin for React js framework of [tawk.to](https://www.tawk.to/) messenger.
+
+## Getting Started
+The plugin is available in node package manager.
+```bash
+# Node
+npm install tawk-messenger-react
+
+# Yarn
+yarn add tawk-messenger-react
+```
+
+## Documentation
+
+This project includes a `docs` folder with more details on:
+1.  [How to Use](docs/how-to-use.md)
+1.  [API Reference](docs/api-reference.md)
+
+## Frequently Asked Questions
+
+**Where you can approach us for suggestion or bug report?**
+
+You can mention us on [Twitter](https://twitter.com/tawktotawk) or in our [Website](https://www.tawk.to/) just message us.
+
+## Maintained by
+<table>
+    <tr>
+        <td align="center"><a href="https://github.com/tawk"><img height="100px" width="100px" src="https://avatars.githubusercontent.com/u/9743939?s=200&v=4" style="max-width: 100%;"><br><sub><b>Tawk Developers</b></sub></a></td>
+    </tr>
+</table>
---
+++ src/index.js
@@ -0,0 +1,293 @@
+import { forwardRef, useEffect, useImperativeHandle } from 'react';
+import PropTypes from 'prop-types';
+
+const TawkMessenger = forwardRef((props, ref) => {
+	useEffect(() => {
+		load();
+	});
+
+	const load = () => {
+		if (!window || !document) {
+			return;
+		}
+
+		/**
+		 * Set placeholder
+		 */
+		window.Tawk_API = window.Tawk_API || {};
+
+		/**
+		 * Inject the Tawk script
+		 */
+		loadScript();
+
+		init();
+	}
+
+	const init = () => {
+		if (props.customStyle && typeof props.customStyle === 'object') {
+			window.Tawk_API.customStyle = props.customStyle;
+		}
+
+		mapCallbacks();
+	}
+
+	const loadScript = () => {
+		var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+
+		const script = document.createElement('script');
+		script.async = true;
+		script.src = `https://embed.talk.lv/${props.propertyId}/${props.widgetId}`;
+		script.charset = 'UTF-8';
+		script.setAttribute('crossorigin', '*');
+
+		const firstScript = document.getElementsByTagName('script')[0];
+		firstScript.parentNode.insertBefore(script, firstScript);
+	}
+
+	
+	useImperativeHandle(ref, () => ({
+		/**
+		 * API for calling an action on the widget
+		 */
+		maximize : () => { 
+			return window.Tawk_API.maximize();
+		},
+
+		minimize : () => {
+			return window.Tawk_API.minimize();
+		},
+
+		toggle : () => {
+			return window.Tawk_API.toggle();
+		},
+
+		popup : () => {
+			return window.Tawk_API.popup();
+		},
+
+		showWidget : () => {
+			return window.Tawk_API.showWidget();
+		},
+
+		hideWidget : () => {
+			return window.Tawk_API.hideWidget();
+		},
+
+		toggleVisibility : () => {
+			return window.Tawk_API.toggleVisibility();
+		},
+
+		endChat : () => {
+			return window.Tawk_API.endChat();
+		},
+
+		
+		/**
+		 * API for returning a data
+		 */
+		getWindowType : () => {
+			return window.Tawk_API.getWindowType();
+		},
+
+		getStatus : () => {
+			return window.Tawk_API.getStatus();
+		},
+
+		isChatMaximized : () => {
+			return window.Tawk_API.isChatMaximized();
+		},
+
+		isChatMinimized : () => {
+			return window.Tawk_API.isChatMinimized();
+		},
+
+		isChatHidden : () => {
+			return window.Tawk_API.isChatHidden();
+		},
+
+		isChatOngoing : () => {
+			return window.Tawk_API.isChatOngoing();
+		},
+
+		isVisitorEngaged : () => {
+			return window.Tawk_API.isVisitorEngaged();
+		},
+
+		onLoaded : () => {
+			return window.Tawk_API.onLoaded;
+		},
+
+
+		/**
+		 * API for setting a data on the widget
+		 */
+		visitor : (data) => {
+			window.Tawk_API.visitor = data;
+		},
+
+		setAttributes : (attribute, callback) => {
+			window.Tawk_API.setAttributes(attribute, callback);
+		},
+
+		addEvent : (event, metadata, callback) => {
+			window.Tawk_API.addEvent(event, metadata, callback);
+		},
+
+		addTags : (tags, callback) => {
+			window.Tawk_API.addTags(tags, callback);
+		},
+
+		removeTags : (tags, callback) => {
+			window.Tawk_API.removeTags(tags, callback);
+		}
+	}));
+
+	/**
+	 * API for listening an event emitting
+	 * inside of the widget
+	 */
+	const mapCallbacks = () => {
+		window.addEventListener('tawkLoad', () => {
+			props.onLoad();
+		});
+
+		window.addEventListener('tawkStatusChange', (status) => {
+			props.onStatusChange(status.detail);
+		});
+
+		window.addEventListener('tawkBeforeLoad', () => {
+			props.onBeforeLoad();
+		});
+
+		window.addEventListener('tawkChatMaximized', () => {
+			props.onChatMaximized();
+		});
+
+		window.addEventListener('tawkChatMinimized', () => {
+			props.onChatMinimized();
+		});
+
+		window.addEventListener('tawkChatHidden', () => {
+			props.onChatHidden();
+		});
+
+		window.addEventListener('tawkChatStarted', () => {
+			props.onChatStarted();
+		});
+
+		window.addEventListener('tawkChatEnded', () => {
+			props.onChatEnded();
+		});
+
+		window.addEventListener('tawkPrechatSubmit', (data) => {
+			props.onPrechatSubmit(data.detail);
+		});
+
+		window.addEventListener('tawkOfflineSubmit', (data) => {
+			props.onOfflineSubmit(data.detail);
+		});
+		
+		window.addEventListener('tawkChatMessageVisitor', (message) => {
+			props.onChatMessageVisitor(message.detail);
+		});
+
+		window.addEventListener('tawkChatMessageAgent', (message) => {
+			props.onChatMessageAgent(message.detail);
+		});
+
+		window.addEventListener('tawkChatMessageSystem', (message) => {
+			props.onChatMessageSystem(message.detail);
+		});
+
+		window.addEventListener('tawkAgentJoinChat', (data) => {
+			props.onAgentJoinChat(data.detail);
+		});
+		
+		window.addEventListener('tawkAgentLeaveChat', (data) => {
+			props.onAgentLeaveChat(data.detail);
+		});
+
+		window.addEventListener('tawkChatSatisfaction', (satisfaction) => {
+			props.onChatSatisfaction(satisfaction.detail);
+		});
+		
+		window.addEventListener('tawkVisitorNameChanged', (visitorName) => {
+			props.onVisitorNameChanged(visitorName.detail);
+		});
+
+		window.addEventListener('tawkFileUpload', (link) => {
+			props.onFileUpload(link.detail);
+		});
+
+		window.addEventListener('tawkTagsUpdated', (data) => {
+			props.onTagsUpdated(data.detail);
+		});
+	}
+
+	return null;
+});
+
+
+TawkMessenger.defaultProps = {
+	customStyle : {},
+	onLoad : () => {},
+	onStatusChange : () => {},
+	onBeforeLoad : () => {},
+	onChatMaximized : () => {},
+	onChatMinimized : () => {},
+	onChatHidden : () => {},
+	onChatStarted : () => {},
+	onChatEnded : () => {},
+	onPrechatSubmit : () => {},
+	onOfflineSubmit : () => {},
+	onChatMessageVisitor : () => {},
+	onChatMessageAgent : () => {},
+	onChatMessageSystem : () => {},
+	onAgentJoinChat : () => {},
+	onAgentLeaveChat : () => {},
+	onChatSatisfaction : () => {},
+	onVisitorNameChanged : () => {},
+	onFileUpload : () => {},
+	onTagsUpdated : () => {}
+}
+
+
+TawkMessenger.propTypes = {
+	/**
+	 * Default properties
+	 */
+	propertyId : PropTypes.string.isRequired,
+	widgetId : PropTypes.string.isRequired,
+
+	/**
+	 * Custom style
+	 */
+	customStyle : PropTypes.object,
+
+	/**
+	 * Callbacks
+	 */
+	onLoad : PropTypes.func,
+	onStatusChange : PropTypes.func,
+	onBeforeLoad : PropTypes.func,
+	onChatMaximized : PropTypes.func,
+	onChatMinimized : PropTypes.func,
+	onChatHidden : PropTypes.func,
+	onChatStarted : PropTypes.func,
+	onChatEnded : PropTypes.func,
+	onPrechatSubmit : PropTypes.func,
+	onOfflineSubmit : PropTypes.func,
+	onChatMessageVisitor : PropTypes.func,
+	onChatMessageAgent : PropTypes.func,
+	onChatMessageSystem : PropTypes.func,
+	onAgentJoinChat : PropTypes.func,
+	onAgentLeaveChat : PropTypes.func,
+	onChatSatisfaction : PropTypes.func,
+	onVisitorNameChanged : PropTypes.func,
+	onFileUpload : PropTypes.func,
+	onTagsUpdated : PropTypes.func
+};
+
+
+export default TawkMessenger;
