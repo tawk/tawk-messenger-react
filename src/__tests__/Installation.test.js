98626c84df70d45c2e1f8fbe63111dda0e66cea0
Jerald Austero <jerald.austero@tawk.to>
2021-12-09 23:46:03 +0800

    Write a test for installation

---
+++ src/__tests__/Installation.test.js
@@ -0,0 +1,100 @@
+// Dependecy
+import { unmountComponentAtNode } from 'react-dom';
+import { render, act } from '@testing-library/react'
+
+// Plugin
+import TawkMessengerReact from '../index';
+
+
+let container = null;
+jest.spyOn(console, 'error').mockReturnValue(true);
+
+
+describe('Install tawk-messenger-react', () => {
+	beforeEach(() => {
+		container = document.createElement('script');
+		document.body.appendChild(container);
+	})
+
+	afterEach(() => {
+		unmountComponentAtNode(container);
+		container.remove();
+		container = null;
+	});
+
+	describe('Should throw an error in missing required properties', () => {
+		it('when propertyId and widgetId are not defined', () => {
+			act(() => {
+				render(<TawkMessengerReact/>, container);
+			})
+
+			/**
+			 * 1st error - PropType 'propertyId' is required
+			 * 2nd error - PropType 'widgetId' is required
+			 * 3rd error - Custom log from load()
+			 */
+			expect(console.error).toHaveBeenCalledTimes(3);
+		});
+
+		it('when widgetId only is not defined', () => {
+			act(() => {
+				render(
+					<TawkMessengerReact
+						propertyId="property_Id"/>,
+					container
+				);
+			})
+
+			/**
+			 * 1st error - PropType 'widgetId' is required
+			 * 2nd error - Custom log from load()
+			 */
+			expect(console.error).toHaveBeenCalledTimes(1);
+		});
+
+		it('when propertId only is not defined', () => {
+			act(() => {
+				render(
+					<TawkMessengerReact
+						widgetId="widget_id"/>,
+					container
+				);
+			});
+
+			/**
+			 * 1st error - PropType 'widgetId' is required
+			 */
+			expect(console.error).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	
+	describe('Initialize plugin', () => {
+		it('should have global variable \'Tawk_API\'', () => {
+			act(() => {
+				render(
+					<TawkMessengerReact
+						propertyId="property_id"
+						widgetId="widget_id"/>,
+					container
+				);
+			});
+			
+			expect(typeof window.Tawk_API === 'object').toBe(true);
+		});
+
+		it('should set \'customStyle\' property in \'Tawk_API\'', () => {
+			act(() => {
+				render(
+					<TawkMessengerReact
+						propertyId="property_id"
+						widgetId="widget_id"
+						customStyle={{}}/>,
+					container
+				);
+			});
+			
+			expect(typeof window.Tawk_API.customStyle === 'object').toBe(true);
+		});
+	});
+});
